787fa6ef660300be994081f04d3a85914bb3ad53
gimgisu <gisu1102@gmail.com>
2025-05-19 23:24:16 +0900

    fix : routeRouter

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    root_path="/recommend",
-    docs_url="/docs",
-    openapi_url="/openapi.json",
-    title="Nutrition Recommendation API",
-    description="추천 알고리즘을 이용해 개인 맞춤 음식 리스트를 반환합니다.",
-    version="1.0.0"
-)
+app = FastAPI()
