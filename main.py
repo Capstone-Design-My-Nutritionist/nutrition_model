91be94543fe55d4f7e3336d6f6c2a6d58183a8ea
gimgisu <gisu1102@gmail.com>
2025-05-19 23:05:43 +0900

    fix : swagger Path

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI(root_path="/recommend")
+app = FastAPI(
+    root_path="/recommend",
+    docs_url="/docs",
+    openapi_url="/openapi.json",
+    title="Nutrition Recommendation API",
+    description="추천 알고리즘을 이용해 개인 맞춤 음식 리스트를 반환합니다.",
+    version="1.0.0"
+)
